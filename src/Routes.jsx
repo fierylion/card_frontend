1ba327377c0a84d868b292b778f23cc7695091d3
fierylion <mawalladaniel2018@gmail.com>
2023-08-02 07:39:09 +0300

    first commit

---
+++ src/Routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+const Routes = () => {
+  return (
+   <React.Suspense fallback={<div>Loading...</div>}>
+    <Router>
+    
+      </Router>
+
+    </React.Suspense>
+  )
+}
+
+export default Routes
---
+++ src/pages/OpenPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OpenPage = () => {
+  return (
+    <div>OpenPage</div>
+  )
+}
+
+export default OpenPage
